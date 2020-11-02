6c2bac906275e9f7f4b463bbe6db6caa2ad93bbd
cihankarluk <cihan.karluk@akinon.com>
2020-11-02 14:33:15 +0300

    TVFY: created artist app

---
+++ TvFY/artist/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Artist(models.Model):
+    name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+    image = models.URLField(null=True, blank=True)
+    awards = models.IntegerField(default=0)
