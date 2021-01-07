96ee26737ce77044b96a0324530063ac860c1de5
cihankarluk <cihan.karluk@akinon.com>
2021-01-08 00:03:30 +0300

    TVFY: add base scrapper class

--- TvFY/collector/base.py
+++ TvFY/collector/base.py
@@ -1,15 +1,32 @@
 import asyncio
 import logging
+from urllib.parse import urlparse
+
 import aiohttp
 from bs4 import BeautifulSoup
+
+from TvFY.collector.imdb import IMDBScrapper
+from TvFY.collector.tomatoes import TomatoesScrapper
 
 logger = logging.getLogger('main')
 
 
 class Scrapper:
-    def __init__(self, urls):
-        self.urls = urls
+    def __init__(self, urls, search_type):
+        self.urls: list = urls
         self.session = None
+        self.search_type = search_type
+
+    @staticmethod
+    def scrapper_class(url, soup, search_type):
+        scrapper_map = {
+            "www.rottentomatoes.com": TomatoesScrapper,
+            "www.imdb.com": IMDBScrapper
+        }
+        base_url = urlparse(url).netloc
+        if cls := scrapper_map.get(base_url):
+            cls = cls(soup=soup[url], url=url, search_type=search_type)
+        return cls
 
     async def fetch_html(self, url: str):
         search_response = await self.session.get(url)
@@ -20,15 +37,20 @@ class Scrapper:
             return await self.fetch_html(url)
         return search_response
 
-    async def soup_response(self, url):
+    async def soup_response(self, url: str) -> dict:
         search_response = await self.fetch_html(url=url)
         text = await search_response.text()
-        soup = BeautifulSoup(text, "lxml")
+        soup = BeautifulSoup(text, "html.parser")
         return {url: soup}
+
+    async def weed_out(self, url: str):
+        soup = await self.soup_response(url=url)
+        cls = self.scrapper_class(url, soup, self.search_type)
+        return cls.run()
 
     async def run(self):
         results = []
-        tasks = [self.soup_response(url=url) for url in self.urls]
+        tasks = [self.weed_out(url=url) for url in self.urls]
         for task in asyncio.as_completed(tasks):
             soup = await task
             results.append(soup)
@@ -39,7 +61,8 @@ class Scrapper:
                 connector=aiohttp.TCPConnector(ssl=False),
         ) as session:
             self.session = session
-            return await self.run()
+            response = await self.run()
+            return response
 
     def handle(self):
         return asyncio.run(self.main())
