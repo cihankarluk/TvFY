b97eaa22e56214a1793e2fda593e96315c648b33
cihankarluk <cihan.karluk@akinon.com>
2021-01-17 00:02:30 +0300

    TVFY: add rewards for imdb urls

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('TvFY.account.urls'), name='account'),
-
+    path('search/', include('TvFY.search.urls'), name='search')
 ]
