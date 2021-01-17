9c7b191ab92c9221dd392aae0d8d5e613b9fef09
cihankarluk <cihan.karluk@akinon.com>
2021-01-17 19:37:07 +0300

    TVFY: add genre model and migrations

--- TvFY/genre/migrations/0001_initial.py
+++ TvFY/genre/migrations/0001_initial.py
@@ -16,6 +16,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=128)),
+                ('detail', models.TextField())
             ],
         ),
     ]
--- TvFY/genre/models.py
+++ TvFY/genre/models.py
@@ -3,3 +3,7 @@ from django.db import models
 
 class Genre(models.Model):
     name = models.CharField(max_length=128)
+    detail = models.TextField()
+
+    def __str__(self):
+        return self.name
